0414c4657970794f576f9a4463d498000cd18b72
faizbyp <faizbyp@gmail.com>
2024-06-04 22:42:36 +0700

    change css; enable caching

--- script.js
+++ script.js
@@ -2,16 +2,35 @@ function capitalize(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+function getPokemon(poke) {
+  // $.get(`https://pokeapi.co/api/v2/pokemon/${poke}`, function (data, status) {
+  //   if (status === "success") {
+  //     console.log(data);
+  //     $(`#${poke} > img`).attr("src", data.sprites.front_default).hide().fadeIn(2000);
+  //     $(`#${poke} > h3`).append(capitalize(data.name));
+  //   } else {
+  //     $(`#${poke}`).append("Cannot get data");
+  //   }
+  // });
+
+  // ENABLE CACHING
+  $.ajax({
+    url: `https://pokeapi.co/api/v2/pokemon/${poke}`,
+    method: "GET",
+    cache: true,
+    success: function (data) {
+      // console.log(data);
+      $(`#${poke} > img`).attr("src", data.sprites.front_default);
+      $(`#${poke} > img`).attr("alt", data.name);
+      $(`#${poke} > h3`).append(capitalize(data.name));
+    },
+  });
+}
+
 $(document).ready(function () {
   $("#hero > *").hide().fadeIn(1000);
 
-  $.get("https://pokeapi.co/api/v2/pokemon/treecko", function (data, status) {
-    if (status === "success") {
-      console.log(data);
-      $("#treecko > div > img").attr("src", data.sprites.front_default).hide().fadeIn(2000);
-      $("#treecko > div > h2").append(capitalize(data.name));
-    } else {
-      $("#treecko").append("Cannot get data");
-    }
-  });
+  getPokemon("treecko");
+  getPokemon("torchic");
+  getPokemon("mudkip");
 });
