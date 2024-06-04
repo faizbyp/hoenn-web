eacba1366eef94d6af51120b6859a03c2e09d195
faizbyp <faizbyp@gmail.com>
2024-06-04 23:11:46 +0700

    modularize css

--- script.js
+++ script.js
@@ -3,23 +3,12 @@ function capitalize(string) {
 }
 
 function getPokemon(poke) {
-  // $.get(`https://pokeapi.co/api/v2/pokemon/${poke}`, function (data, status) {
-  //   if (status === "success") {
-  //     console.log(data);
-  //     $(`#${poke} > img`).attr("src", data.sprites.front_default).hide().fadeIn(2000);
-  //     $(`#${poke} > h3`).append(capitalize(data.name));
-  //   } else {
-  //     $(`#${poke}`).append("Cannot get data");
-  //   }
-  // });
-
   // ENABLE CACHING
   $.ajax({
     url: `https://pokeapi.co/api/v2/pokemon/${poke}`,
     method: "GET",
     cache: true,
     success: function (data) {
-      // console.log(data);
       $(`#${poke} > img`).attr("src", data.sprites.front_default);
       $(`#${poke} > img`).attr("alt", data.name);
       $(`#${poke} > h3`).append(capitalize(data.name));
