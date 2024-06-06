d9df52f04b99596ed14688b6c18804c0e2fbd70e
faizbyp <faizbyp@gmail.com>
2024-06-06 22:02:35 +0700

    feat: poke evolved when button clicked

--- script.js
+++ script.js
@@ -2,33 +2,27 @@ function capitalize(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-function getPokemon(poke) {
-  // ENABLE CACHING
+function getPokemon(type, poke) {
   $.ajax({
     url: `https://pokeapi.co/api/v2/pokemon/${poke}`,
     method: "GET",
     cache: true,
     success: function (data) {
-      $(`#${poke} > img`).attr("src", data.sprites.front_default);
-      $(`#${poke} > img`).attr("alt", data.name);
-      $(`#${poke} > h3`).append(capitalize(data.name));
+      $(`#${type} > img`).attr("src", data.sprites.front_default);
+      $(`#${type} > img`).attr("alt", data.name);
+      $(`#${type} > h3`).text(capitalize(data.name));
 
       const types = data.types;
       console.log(types);
+      $(`#${type} > span`).remove();
       types.forEach(function (t) {
-        $(`#${poke}`).append(`<span class="pill">${capitalize(t.type.name)}</span>`);
+        $(`#${type}`).append(`<span class="pill">${capitalize(t.type.name)}</span>`);
       });
     },
   });
 }
 
-$(document).ready(function () {
-  $("#hero > *").hide().fadeIn(1000);
-
-  getPokemon("treecko");
-  getPokemon("torchic");
-  getPokemon("mudkip");
-
+function getLocations() {
   $.ajax({
     url: "https://pokeapi.co/api/v2/location?offset=417&limit=30",
     method: "GET",
@@ -51,4 +45,32 @@ $(document).ready(function () {
       $("<li>and many more</li>").attr("class", "loc-item").appendTo("#locations");
     },
   });
+}
+
+$(document).ready(function () {
+  $("#hero > *").hide().fadeIn(1000);
+
+  const grassPoke = ["treecko", "grovyle", "sceptile"];
+  const firePoke = ["torchic", "combusken", "blaziken"];
+  const waterPoke = ["mudkip", "marshtomp", "swampert"];
+  var pokeIndex = 0;
+
+  getPokemon("grass", grassPoke[pokeIndex]);
+  getPokemon("fire", firePoke[pokeIndex]);
+  getPokemon("water", waterPoke[pokeIndex]);
+
+  $("#evolve").on("click", function () {
+    if (pokeIndex === 1) {
+      $(this).text("Back to not evolved");
+    } else {
+      $(this).text("Evolve");
+    }
+
+    pokeIndex = (pokeIndex + 1) % 3;
+    getPokemon("grass", grassPoke[pokeIndex]);
+    getPokemon("fire", firePoke[pokeIndex]);
+    getPokemon("water", waterPoke[pokeIndex]);
+  });
+
+  getLocations();
 });
